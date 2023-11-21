c20f01309dec41fbc113538e133864b72ac2dc36
TeohS4 <76810590+TeohS4@users.noreply.github.com>
2023-11-21 18:03:42 +0800

    Update login.php

--- public_html/admin/login.php
+++ public_html/admin/login.php
@@ -7,59 +7,69 @@ $err = "";
 session_start();
 
 if (isset($_SESSION['admin_id'])) {
-    session_destroy();
+  session_destroy();
 } else {
-    if (isset($_POST['submitted'])) {
-        // Validate and sanitize user input
-        $admin_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        $admin_password = $_POST['password']; // No need to sanitize password for SQL queries
+  if (isset($_POST['submitted'])) {
+    // Validate and sanitize user input
+    $admin_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $admin_password = $_POST['password']; // No need to sanitize password for SQL queries
 
-        if (!filter_var($admin_email, FILTER_VALIDATE_EMAIL)) {
-            $err = "Invalid email format";
-        } else {
-            $dbc = mysqli_connect('localhost', 'root', '');
-            mysqli_select_db($dbc, 'in_haus');
+    if (!filter_var($admin_email, FILTER_VALIDATE_EMAIL)) {
+      $err = "Invalid email format";
+    } else {
+      $dbc = mysqli_connect('localhost', 'root', '');
+      mysqli_select_db($dbc, 'in_haus');
 
-            // Use prepared statements to prevent SQL injection
-            $query = "SELECT * FROM user 
+      // Use prepared statements to prevent SQL injection
+      $query = "SELECT * FROM user 
                         WHERE email = ? 
                         AND access_level != 'Normal User'";
 
-            $stmt = mysqli_prepare($dbc, $query);
-            mysqli_stmt_bind_param($stmt, "s", $admin_email);
-            mysqli_stmt_execute($stmt);
-            $r = mysqli_stmt_get_result($stmt);
+      $stmt = mysqli_prepare($dbc, $query);
+      mysqli_stmt_bind_param($stmt, "s", $admin_email);
+      mysqli_stmt_execute($stmt);
+      $r = mysqli_stmt_get_result($stmt);
 
-            if ($row = mysqli_fetch_assoc($r)) {
-                // Use password_verify to check if the entered password matches the stored hashed password
-                if (password_verify($admin_password, $row['password'])) {
-                    $_SESSION['admin_id'] = $row['user_id'];
-                    $_SESSION['admin_name'] = $row['name'];
-                    $_SESSION['admin_email'] = $row['email'];
-                    $_SESSION['admin_position'] = $row['access_level'];
+      if ($row = mysqli_fetch_assoc($r)) {
+        // Use password_verify to check if the entered password matches the stored hashed password
+        if (password_verify($admin_password, $row['password'])) {
+          $_SESSION['admin_id'] = $row['user_id'];
+          $_SESSION['admin_name'] = $row['name'];
+          $_SESSION['admin_email'] = $row['email'];
+          $_SESSION['admin_position'] = $row['access_level'];
 
-                    if ($_SESSION['admin_position'] == "Customer Service") {
-                        header("Location: users.php");
-                    } else {
-                        header("Location: dashboard.php");
-                    }
-                } else {
-                    $err = "The e-mail or password that you've entered is incorrect. Please try again.";
-                }
-            } else {
-                $err = "$admin_email - This email does not exist!";
-            }
+          date_default_timezone_set("Asia/Singapore");
+          // Log admin login activity into log_activity table
+          $user_id = $row['user_id']; 
+          $current_datetime = date("Y-m-d H:i:s"); 
 
-            mysqli_close($dbc);
+          // Insert into log_activity table
+          $insert_query = "INSERT INTO log_activity (event_type, user_id, datetime) VALUES ('admin_login', ?, ?)";
+          $insert_stmt = mysqli_prepare($dbc, $insert_query);
+          mysqli_stmt_bind_param($insert_stmt, "ss", $user_id, $current_datetime);
+          mysqli_stmt_execute($insert_stmt);
+
+          if ($_SESSION['admin_position'] == "Customer Service") {
+            header("Location: users.php");
+          } else {
+            header("Location: dashboard.php");
+          }
+        } else {
+          $err = "The e-mail or password that you've entered is incorrect. Please try again.";
         }
+      } else {
+        $err = "$admin_email - This email does not exist!";
+      }
+
+      mysqli_close($dbc);
     }
+  }
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <?php include("head.php"); ?>
 
@@ -73,16 +83,20 @@ if (isset($_SESSION['admin_id'])) {
   <!-- Template CSS -->
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/components.css">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <!-- Start GA -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-94034622-3');
+  </script>
+  <!-- /END GA -->
+</head>
 
 <body>
   <div id="app">
@@ -112,7 +126,7 @@ if (isset($_SESSION['admin_id'])) {
 
                   <div class="form-group">
                     <div class="d-block">
-                    	<label for="password" class="control-label">Password</label>
+                      <label for="password" class="control-label">Password</label>
                     </div>
                     <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
                     <div class="invalid-feedback">
@@ -151,13 +165,14 @@ if (isset($_SESSION['admin_id'])) {
   <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
   <script src="assets/modules/moment.min.js"></script>
   <script src="assets/js/stisla.js"></script>
-  
+
   <!-- JS Libraies -->
 
   <!-- Page Specific JS File -->
-  
+
   <!-- Template JS File -->
   <script src="assets/js/scripts.js"></script>
   <script src="assets/js/custom.js"></script>
 </body>
+
 </html>
